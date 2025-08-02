965eed8653cd264f6f55d7b3290cd03b0486e39a
Ysabelle <yskim@csumb.edu>
2025-08-01 19:30:25 -0700

    new branch vehicle_groundwork - added VehicleActivity.java, activity_vehicle.xml - added build.gradle buildFeatures: viewbinding, databinding - updated MainActivity.java to have intentFactory

--- app/src/main/java/com/stanissudo/jycs_crafters/MainActivity.java
+++ app/src/main/java/com/stanissudo/jycs_crafters/MainActivity.java
@@ -1,6 +1,11 @@
 package com.stanissudo.jycs_crafters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.stanissudo.jycs_crafters.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.wk05hw03_gymlog.MAIN_ACTIVITY_USER_ID";
+
+    private ActivityMainBinding binding;
+
+    // change to be the Repository type
+    //private <<GasAppRepository>> repository;
+
+    /**
+     * onCreate() creates MainActivity activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // TODO: implement click button, "Add Vehicle", and call vehicleIntentFactory
+        /*binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getInformationFromDisplay();
+                //insertGymLogRecord();
+            }
+        });*/
+    }
+
+    /**
+     * mainActivityIntentFactory returns an intent to change the screen
+     *
+     * @param context context
+     * @param userID  userID
+     * @return intent
+     */
+    static Intent mainActivityIntentFactory(Context context, int userID) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userID);
+        return intent;
     }
 }
