2beff5f43d53eb24fcb3ec9444833223ca8fb6d7
mcaicedo-csumb <mcaicedo@csumb.edu>
2025-08-04 22:54:18 -0700

    Update UserManagementActivity.java

--- app/src/main/java/com/stanissudo/jycs_crafters/UserManagementActivity.java
+++ app/src/main/java/com/stanissudo/jycs_crafters/UserManagementActivity.java
@@ -7,6 +7,6 @@ public class UserManagementActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_management); // ✅ Link your XML
+        setContentView(R.layout.activity_user_management);
     }
 }
