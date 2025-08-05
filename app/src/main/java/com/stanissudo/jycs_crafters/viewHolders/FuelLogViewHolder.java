2400764ba7c7cc985320bcb073e5e9570128edf9
Ysabelle <yskim@csumb.edu>
2025-08-04 20:59:45 -0700

    updated files: - FuelLogAdapter.java implemented - FuelLogViewHolder.java implemented - activity_main.xml edits - strings.xml added strings for activity_main.xml

--- app/src/main/java/com/stanissudo/jycs_crafters/viewHolders/FuelLogViewHolder.java
+++ app/src/main/java/com/stanissudo/jycs_crafters/viewHolders/FuelLogViewHolder.java
@@ -1,5 +1,14 @@
 package com.stanissudo.jycs_crafters.viewHolders;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.stanissudo.jycs_crafters.R;
+
 /**
  * @author Ysabelle Kim
  * created: 8/4/2025 - 8:10 PM
@@ -8,6 +17,21 @@ package com.stanissudo.jycs_crafters.viewHolders;
  * @since 1.0.0
  * Explanation: populates the recycler
  */
-public class FuelLogViewHolder {
+public class FuelLogViewHolder extends RecyclerView.ViewHolder {
+    private final TextView fuelLogViewItem;
 
+    private FuelLogViewHolder(View fuelLogView) {
+        super(fuelLogView);
+        fuelLogViewItem = fuelLogView.findViewById(R.id.recyclerItemTextView);
+    }
+
+    public void bind(String text) {
+        fuelLogViewItem.setText(text);
+    }
+
+    static FuelLogViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fuel_log_recycler_item, parent, false);
+        return new FuelLogViewHolder(view);
+    }
 }
