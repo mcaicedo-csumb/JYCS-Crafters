f5b9711715eb8db4a9b4fbeea15092eac6831c5a
Ysabelle <yskim@csumb.edu>
2025-08-04 20:46:00 -0700

    new files: - FuelLogAdapter.java - FuelLogViewHolder.java - FuelLogViewModel.java - fuel_log_recycler_item.xml updated files: - changed android theme in AndroidManifest.xml - activity_vehicle.xml layout - activity_main.xml edits - strings.xml added strings for activity_vehicle.xml

---
+++ app/src/main/java/com/stanissudo/jycs_crafters/viewHolders/FuelLogAdapter.java
@@ -0,0 +1,12 @@
+package com.stanissudo.jycs_crafters.viewHolders;
+
+/**
+ * @author Ysabelle Kim
+ * created: 8/4/2025 - 8:09 PM
+ * @project JYCS-Crafters
+ * file: FuelLogAdapter.java
+ * @since 1.0.0
+ * Explanation: adapter for
+ */
+public class FuelLogAdapter {
+}
---
+++ app/src/main/java/com/stanissudo/jycs_crafters/viewHolders/FuelLogViewHolder.java
@@ -0,0 +1,13 @@
+package com.stanissudo.jycs_crafters.viewHolders;
+
+/**
+ * @author Ysabelle Kim
+ * created: 8/4/2025 - 8:10 PM
+ * @project JYCS-Crafters
+ * file: FuelLogViewHolder.java
+ * @since 1.0.0
+ * Explanation: populates the recycler
+ */
+public class FuelLogViewHolder {
+
+}
